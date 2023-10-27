4450ac7bbdf1962de38cbc91455aea179ed452e3
Aly M. Mobarak <aly.m.s.mobarak@gmail.com>
2023-10-27 17:13:34 +0200

    Updated Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Image from 'next/image' // use instead of <img>
+
 export default function Home() {
   return (
-    <article className="prose">
-      <h1>Initial Commit</h1>
+    <center><article className="prose w-screen flex flex-col items-center mt-10">
+      <h1 className="m-0">Initial Commit</h1>
 
-      <p>Welcome to this page! If you've made it here, well done!</p>
-    </article>
+      <p>Welcome to this page! Well done for making it here!</p>
+    </article></center>
   )
 }
