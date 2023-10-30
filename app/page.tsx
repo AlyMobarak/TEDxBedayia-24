d073358377f4140d7fc749e42c812b4bfd5d1c15
Aly M. Mobarak <aly.m.s.mobarak@gmail.com>
2023-10-30 23:27:46 +0200

    Included Hero Section after greeting

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+import HeroSection from '@/views/Home/Hero Section/hero'
 import Image from 'next/image' // use instead of <img>
 
 export default function Home() {
   return (
-    <center><article className="prose w-screen flex flex-col items-center mt-10">
-      <h1 className="m-0">Initial Commit</h1>
+    <center>
+      <article className="prose w-screen flex flex-col items-center mt-10">
+        <h1 className="m-0">Initial Commit</h1>
 
-      <p>Welcome to this page! Well done for making it here!</p>
-    </article></center>
+        <p>Welcome to this page! Well done for making it here!</p>
+      </article>
+
+      <br />
+      
+      <HeroSection />
+
+    </center>
   )
 }
