2653a5179aabc565f3a50fde7baddc9f96e597a3
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-01-12 23:16:18 +0200

    Changed from <article> to <section>.

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image' // use instead of <img>
 export default function Home() {
   return (
     <center>
-      <article className="prose w-screen flex flex-col items-center mt-10">
+      <section className="prose w-screen flex flex-col items-center mt-10">
         <h1 className="m-0">Initial Commit</h1>
 
         <p>Welcome to this page! Well done for making it here!</p>
-      </article>
+      </section>
 
       <br />
       
--- views/Home/Hero Section/hero.tsx
+++ views/Home/Hero Section/hero.tsx
@@ -1,9 +1,9 @@
 export default function HeroSection() {
   return (
-    <center><article id="hero-section" className="prose w-screen flex flex-col items-center mt-10">
+    <center><section id="hero-section" className="prose w-screen flex flex-col items-center mt-10">
       <h1 className="m-0">Sample Section</h1>
 
       <p>It's nice having you here!</p>
-    </article></center>
+    </section></center>
   )
 }
