eca5e0abba4e21ab3fcf53126c89d5648235e507
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-01-12 23:12:25 +0200

    Added Colors to TailWindCSS Config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,48 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        transparent: 'transparent',
+        'background': '#100d26',
+        'text': '#FFFFFF',
+        'text-gray': {
+          'light': '#DFDFDF',
+          'dark': '#BABABA',
+        },
+        'textbox': '#b9b3e540',
+        'primary': {
+          100: '#FFE6EE',
+          200: '#E5B3C3',
+          300: '#CA7892',
+          400: '#B04C6B',
+          default: '#8D2547',
+          600: '#7B1A37',
+          700: '#610E27',
+          800: '#47081A',
+          900: '#2B050F',
+        },
+        'secondary': {
+          100: '#E9E6FF',
+          200: '#B9B3E5',
+          300: '#8278CA',
+          400: '#584CB0',
+          default: '#32258D',
+          600: '#251A7B',
+          700: '#180E61',
+          800: '#0F0847',
+          900: '#09052B',
+        },
+        'success': {
+          'light': '#E6FFF2',
+          'dark': '#2BCB77',
+        },
+        'error': {
+          'light': '#FFEBEE',
+          'dark': '#CC2B48',
+        },
+        'accent': '#4636D9',
+        'tedx': '#EB0028',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
