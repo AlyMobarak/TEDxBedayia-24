3323540538c67c278bc4aad2293d7b37167a529a
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-01-13 12:45:37 +0200

    Removed pre-filled content from hero section files.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image' // use instead of <img>
 export default function Home() {
   return (
     <center>
-      <section className="prose w-screen flex flex-col items-center mt-10">
+      <section className="prose w-screen flex flex-col items-center mt-10 font-sans">
         <h1 className="m-0">Initial Commit</h1>
 
         <p>Welcome to this page! Well done for making it here!</p>
--- views/Home/Hero Section/hero.tsx
+++ views/Home/Hero Section/hero.tsx
@@ -1,9 +1,8 @@
 export default function HeroSection() {
   return (
-    <center><section id="hero-section" className="prose w-screen flex flex-col items-center mt-10">
-      <h1 className="m-0">Sample Section</h1>
-
-      <p>It's nice having you here!</p>
-    </section></center>
+    <center>
+      <section id="hero" className="">
+      </section>
+    </center>
   )
 }
