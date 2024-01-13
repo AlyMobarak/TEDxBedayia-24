ad6826ea9c9669b3c2bbb0a64de89e861e0e4bed
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-01-13 15:36:34 +0200

    Testing Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image' // use instead of <img>
 export default function Home() {
   return (
     <center>
-      <section className="prose w-screen flex flex-col items-center mt-10 font-sans">
-        <h1 className="m-0">Initial Commit</h1>
+      <section className="prose w-screen flex flex-col items-center mt-10">
+        <h1 className="m-0 font-body">Initial Commit</h1>
 
         <p>Welcome to this page! Well done for making it here!</p>
       </section>
