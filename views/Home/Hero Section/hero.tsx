1e2d9d3877259b15c97655d46fab42108fbee484
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-01-15 21:07:49 +0200

    Hero Section is now completed. All text is final from marketing team. What remains is making it responsive and the testimonials section (work in progress). For responsiveness, screens under 1350px would get a minified nav bar (not too much except on mobile) and slightly lower font sizes (tablets). For mobiles, next break point when above navbar fails, condense navbar extremely and lower the font sizes a lot.

--- views/Home/Hero Section/hero.tsx
+++ views/Home/Hero Section/hero.tsx
@@ -1,7 +1,20 @@
 export default function HeroSection() {
   return (
     <center>
-      <section id="hero" className="">
+      <section id="hero" className="w-screen pt-[6.75rem] pb-[4.5rem]" style={{background: "url(\"/Hero Section/hero-image.png\") no-repeat top center", backgroundSize: "100vw"}}>
+        <div>
+          <img className="w-[45rem]" src="/Hero Section/main-heading.png" alt="Main Header" />
+          <p className="text-text-gray-light text-center font-body w-[33.5rem]">Buckle up for a journey of inspiration and innovation at TEDx! Immerse yourself in a day filled with thought-provoking conversations, extraordinary performances, and enlightening experiences. Join us as we explore ideas worth spreading. Prepare to connect to a kaleidoscope of minds like your own &ndash; curious & hungry for change. </p>
+
+          <div className="mt-6 flex gap-4 flex-row items-center justify-center">
+            <button className="py-6 justify-center text-center items-center shrink-0 w-[16.25rem] font-button bg-primary rounded-2xl">Book a Ticket</button>
+            <button className="py-5 justify-center text-center items-center shrink-0 w-[16.25rem] font-button rounded-2xl bg-transparent border-4 border-solid border-secondary-400">Book a Group Ticket</button>
+          </div>
+
+          <p className="mt-2 text-center text-text-gray-dark font-body">Await the Wonders! Join 400+ attendees.</p>
+
+          <h4 className="mt-[7rem] min-[1520px]:mt-[9rem] min-[1600px]:mt-[12rem] min-[1720px]:mt-[15rem] min-[1850px]:mt-[18rem] font-title text-center max-w-[34rem]">Speakers, Activities,<br /> and More Coming Out Soon!! Await us!</h4>
+        </div>
       </section>
     </center>
   )
