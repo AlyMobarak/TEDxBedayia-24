19490e409201229b4d3d6f814f65ed19f96ef3cd
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-01-15 21:05:20 +0200

    Removed placeholder text. Added nav bar and some dummy text to test scrolling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
 import HeroSection from '@/views/Home/Hero Section/hero'
+import Navigation from '@/views/Home/Navigation Bar/navigation'
 import Image from 'next/image' // use instead of <img>
 
 export default function Home() {
   return (
     <center>
-      <section className="prose w-screen flex flex-col items-center mt-10">
-        <h1 className="m-0 text-primary">Initial Commit</h1>
-
-        <p>Welcome to this page! Well done for making it here!</p>
-      </section>
-
-      <br />
+      <Navigation />
       
       <HeroSection />
+
+      <p className='mt-[90rem]'>Lol</p>
 
     </center>
   )
