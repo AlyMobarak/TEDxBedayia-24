e1630be1303055bdebc4a5c57a77c5fb88eb4e50
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-01-15 21:03:40 +0200

    Removed buggy import statement and added it to <head>. Changed theme color to background due to primary being distracting. Added line heights and font weights for different font classes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Poppins:wght@400;600;700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap" rel="stylesheet" />
+
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#8D2547"></meta>
+        <meta name="theme-color" content="#100d26"></meta>
         <meta name="author" content="Aly Mobarak" />
         <meta name="description" content="This is the official website for 
         the TEDxYouth event hosted at Bedayia International School. 
