4d3e331595cbc142829a4650c02accc690ec023d
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-01-16 21:28:09 +0200

    Fixes Safari IOS not hiding overflow.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image' // use instead of <img>
 
 export default function Home() {
   return (
-    <center>
+    <center className='wrapper'>
       <Navigation />
       
       <HeroSection />
