abd5b555aedf9963c6512c1fbe2022585102212f
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-01-19 11:02:37 +0200

    Image Optimization Addedd

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import HeroSection from '@/views/Home/Hero Section/hero'
 import Navigation from '@/views/Home/Navigation Bar/navigation'
-import Image from 'next/image' // use instead of <img>
 
 export default function Home() {
   return (
