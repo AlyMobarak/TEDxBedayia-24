f99cba1e7c1ad68f423c69900d5c19bffab3f375
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-01-19 18:59:10 +0200

    d

--- views/Home/Navigation Bar/navigation.tsx
+++ views/Home/Navigation Bar/navigation.tsx
@@ -1,11 +1,15 @@
 "use client"
 
+import { useRouter } from 'next/navigation'
+
 import Link from 'next/link'
 import './navigation.css'
 import { usePathname } from 'next/navigation'
 
 export default function Navigation() {
   const pathname = usePathname();
+  const router = useRouter();
+
   return (
     <center className="w-screen z-50 fixed top-0 backdrop-blur-[5px]" style={{background: "rgba(16, 13, 38, 0.10)"}}>
       <section id="nav" className="flex flex-row max-phone:flex-col items-center justify-between max-phone:justify-center max-w-[1088px] min-w-[1088px] max-phone:max-w-[90vw] max-phone:min-w-[90vw] h-[5.5rem] max-phone:h-[6.625rem]">
@@ -16,7 +20,7 @@ export default function Navigation() {
 
         {/* Links */}
         <div className="nav-links flex flex-row justify-between gap-6 font-body items-center max-phone:hidden">
-          <Link href="">{pathname == "/" ? "Await" : (pathname == "/booking/one" ? "Book a Group Ticket (4)" : "Book one Ticket")}</Link>
+          <Link href={pathname == "/" ? "" /**tocome after replacing await */ : (pathname == "/booking/one" ? "/booking/group" : "/booking/one")}>{pathname == "/" ? "Await" : (pathname == "/booking/one" ? "Book a Group Ticket (4)" : "Book one Ticket")}</Link>
           {/* 15% OFF if on /booking/one */}
           <Link href="" className={(pathname != "/") ? 'hidden' : ''}>the</Link>
           <Link href="" className={(pathname != "/") ? 'hidden' : ''}>full</Link>
@@ -24,7 +28,7 @@ export default function Navigation() {
         </div>
 
         {/* Book a Ticket */}
-        <button className={`bg-primary active:bg-primary-700 hover:bg-primary-600 transition-all flex flex-row items-center py-2 px-6 gap-2 {[max-phone:hidden]} max-phone:mt-4 ${pathname != "/" ? 'hidden' : ''}`} style={{borderRadius: "0.5rem"}}>
+        <button className={`bg-primary active:bg-primary-700 hover:bg-primary-600 transition-all flex flex-row items-center py-2 px-6 gap-2 {[max-phone:hidden]} max-phone:mt-4 ${pathname != "/" ? 'hidden' : ''}`} style={{borderRadius: "0.5rem"}} onClick={() => router.push('/booking/one')}>
           <div className="w-6 h-6"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" style={{width: "100%", height: "100%"}}>
             <path d="M10 14H7" stroke="white" strokeWidth="2" strokeLinecap="round"/>
             <path d="M13 17H7" stroke="white" strokeWidth="2" strokeLinecap="round"/>
