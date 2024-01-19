636057f231bfe57dbc01dc5fe0affe92f9d12da8
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-01-19 10:42:07 +0200

    phone and tablet breakpoints; smoother animation, and brought blurred circles to top.

--- views/Home/Hero Section/hero.tsx
+++ views/Home/Hero Section/hero.tsx
@@ -30,9 +30,9 @@ function blurredCircles() {
 export default function HeroSection() {
   return (
     <center>
+      {blurredCircles()}
       {/* 1200px Nav Bar needs to be condensed a bit and font further lowered. 1350px font lowers and nav bar stays same. */}
       <section id="hero" className="mx-0 w-[100%] max-w-[100vw] pt-[6.75rem] mb-[4.5rem] relative max-phone:bg-[length:80vh_100px]" style={{background: "url(\"/Hero Section/hero-image.png\") no-repeat top center", backgroundSize: "100%"}}>
-        {blurredCircles()}
         <div>
           <img className="w-[45rem] max-phone:w-[95vw]" src="/Hero Section/main-heading.png" alt="Main Header" />
           <p className="text-text-gray-light text-center font-body w-[33.5rem] max-phone:w-[85vw] max-phone:mt-1">Buckle up for a journey of inspiration and innovation at TEDx! Immerse yourself in a day filled with thought-provoking conversations, extraordinary performances, and enlightening experiences. Join us as we explore ideas worth spreading. Prepare to connect to a kaleidoscope of minds like your own &ndash; curious & hungry for change. </p>
