6b4bdc16df41c713eb581293ed1dfd7f999bf3a8
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-01-20 08:31:38 +0200

    phone fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
       <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Poppins:wght@400;600;700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap" rel="stylesheet" />
 
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
         <meta name="theme-color" content="#100d26"></meta>
         <meta name="author" content="Aly Mobarak" />
         <meta name="description" content="This is the official website for 
