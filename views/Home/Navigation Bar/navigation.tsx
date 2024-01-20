d66f5535dfcc2b940925e7ecb699d22896330fab
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-01-20 17:28:13 +0200

    phone fix

--- views/Home/Navigation Bar/navigation.tsx
+++ views/Home/Navigation Bar/navigation.tsx
@@ -19,7 +19,7 @@ export default function Navigation() {
         
 
         {/* Links */}
-        <div className={`nav-links flex flex-row justify-between gap-6 font-body items-center ${pathname == "/" ? "max-phone:hidden" : ""}`}>
+        <div className={`nav-links flex flex-row justify-between gap-6 font-body items-center max-phone:hidden`}>
           <Link href={pathname == "/" ? "" /**tocome after replacing await */ : (pathname == "/booking/one" ? "/booking/group" : "/booking/one")}>{pathname == "/" ? "Await" : (pathname == "/booking/one" ? "Book a Group Ticket (4)" : "Book one Ticket")}</Link>
           {/* 15% OFF if on /booking/one always hidden on phone */}
           <Link href="" className={(pathname != "/") ? 'hidden' : ''}>the</Link>
@@ -28,7 +28,7 @@ export default function Navigation() {
         </div>
 
         {/* Book a Ticket */}
-        <button className={`bg-primary active:bg-primary-700 hover:bg-primary-600 transition-all flex flex-row items-center py-2 px-6 gap-2 {[max-phone:hidden]} max-phone:mt-4 ${pathname != "/" ? 'hidden' : ''}`} style={{borderRadius: "0.5rem"}} onClick={() => router.push('/booking/one')}>
+        <button className={`bg-primary active:bg-primary-700 hover:bg-primary-600 transition-all flex flex-row items-center py-2 px-6 gap-2 max-phone:hidden max-phone:mt-4 ${pathname != "/" ? 'hidden' : ''}`} style={{borderRadius: "0.5rem"}} onClick={() => router.push('/booking/one')}>
           <div className="w-6 h-6"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" style={{width: "100%", height: "100%"}}>
             <path d="M10 14H7" stroke="white" strokeWidth="2" strokeLinecap="round"/>
             <path d="M13 17H7" stroke="white" strokeWidth="2" strokeLinecap="round"/>
