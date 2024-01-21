86240107d2625c65d611a8d0f7e3d1b68969803e
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-01-21 16:01:59 +0200

    a ton of fixes

--- views/Home/Navigation Bar/navigation.tsx
+++ views/Home/Navigation Bar/navigation.tsx
@@ -12,14 +12,14 @@ export default function Navigation() {
 
   return (
     <center className="w-[100%] z-50 fixed top-0 backdrop-blur-[5px]" style={{background: "rgba(16, 13, 38, 0.10)"}}>
-      <section id="nav" className={`flex flex-row max-phone:flex-col items-center ${pathname == "/" ? "justify-between" : "justify-start gap-32"} max-phone:justify-center px-[11rem] max-tablet:px-[6rem] max-phone:px-0 max-phone:w-[80vw] max-phone:max-w-[80vw] max-phone:min-w-[80vw] h-[5.5rem] max-phone:h-[6.625rem]`}>
+      <section id="nav" className={`flex flex-row max-phone:flex-col items-center ${pathname == "/" ? "justify-between" : "justify-start gap-32"} max-phone:justify-center px-[11rem] max-tablet:px-[6rem] max-phone:px-0 max-phone:py-3 max-phone:w-[80vw] max-phone:max-w-[80vw] max-phone:min-w-[80vw] h-[5.5rem] max-phone:h-[6.625rem]`}>
         <Link href="/" className="cursor-pointer hover:opacity-75">
           <img className="h-9 w-[28rem] max-phone:h-[1.6875rem] max-phone:w-[21rem]" src="/Nav Bar/main-logo.png" alt="Event's Logo" />
         </Link>
         
 
         {/* Links */}
-        <div className={`nav-links flex flex-row justify-between gap-6 font-body items-center max-phone:hidden`}>
+        <div className={`nav-links flex flex-row justify-between gap-6 font-body items-center ${pathname == "/" ? "" : "max-phone:hidden"}`}>
           <Link href={pathname == "/" ? "" /**tocome after replacing await */ : (pathname == "/booking/one" ? "/booking/group" : "/booking/one")}>{pathname == "/" ? "Await" : (pathname == "/booking/one" ? "Book a Group Ticket (4)" : "Book one Ticket")}</Link>
           {/* 15% OFF if on /booking/one always hidden on phone */}
           <Link href="" className={(pathname != "/") ? 'hidden' : ''}>the</Link>
