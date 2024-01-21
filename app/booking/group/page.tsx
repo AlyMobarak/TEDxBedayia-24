82af44159a01f23117c05b978d0009fbb92df1d6
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-01-22 00:16:59 +0200

    fix deployment

--- app/booking/group/page.tsx
+++ app/booking/group/page.tsx
@@ -3,7 +3,7 @@ import Navigation from "@/views/Home/Navigation Bar/navigation";
 export default function BookGroupTicketPage() {
   return (
     <center className='wrapper'>
-      {/* <Navigation /> */}
+      <Navigation />
 
     </center>
   )
