854eaa8a524825b616567785620d44da7d1b0406
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-02-06 12:58:22 +0200

    Added Locked Timetable

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,8 @@ export default function Home() {
 
       <Image style={{width: "100vw", marginBottom: "4rem"}} className='mt-8 max-phone:mt-[-5rem]' width="2880" alt="" height="702" src="/confedential-2.png"></Image>
 
+      <Image style={{width: "80vw", marginBottom: "4rem"}} className='mt-16' width="1280" alt="" height="840" src="/locked-timetable.png"></Image>
+
       <div className='mt-20 w-[33.5rem] max-phone:w-[95vw] font-small temp-note text-text-gray-dark'>Send us an email at <a className='font-body text-secondary-200' href="mailto:2hysp8jw62@privaterelay.appleid.com">2hysp8jw62@privaterelay.appleid.com</a> if you find any bugs visually or programatically.</div>
     </center>
   )
