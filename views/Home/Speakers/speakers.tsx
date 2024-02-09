9b23f832238d868eb8047f0d1e633c8eefe13bcd
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2024-02-09 20:05:40 +0200

    Youssef Sabry

--- views/Home/Speakers/speakers.tsx
+++ views/Home/Speakers/speakers.tsx
@@ -51,13 +51,14 @@ function chainedCard() {
 </center>
 }
 export default function Speakers() {
-    return (<section id="speakers" className="mx-0 w-[100%] max-w-[100vw] mt-[-6.75rem] max-phone:pt-[10rem] max-tablet:pt-[5rem] mb-[4.5rem] relative max-phone:bg-[length:80vh_100px]">
+    return (<section id="speakers" className="mx-0 w-[100%] max-w-[100vw] mt-[-6.75rem] max-phone:pt-[10rem] max-tablet:pt-[5rem] mb-[4.5rem]">
         <div id="speakers-title" className='font-title max-phone:margin-negative-top max-phone:mt-[-5rem]'>Speakers</div>
+        <div className='flex flex-row justify-center items-center'><span className="text-center w-screen text-text-gray-dark font-small font-semibold max-phone:hidden">Hover me to view more information!</span></div>
         <div id="speakers-container" className='max-phone:flex-col w-screen max-phone:gap-0 max-phone:p-0 max-phone:m-0'>
             <div id="speakers-container1" className='max-phone:p-0 max-phone:m-0 max-phone:flex-col'>
                 { speakerCard("Ibrahim Abdelfatah", "The Science of Sleep", "Discover the science of the perfect nap with Ibrahim Abdelfattah at TEDx'24! Uncover the latest evidence-based sleep strategies for peak rejuvenation.", "Has the perfect nap been lying in wait for science to unveil its mysteries? Ibrahim Abdelfattah, our first speaker, will crack the science of perfect naps at this year's TEDx. Join us at TEDx'24 & discover evidence-based sleep science secrets!", "/speakers/ibrahim-abdelfatah.png", "bima.abf") }
                 { speakerCard("Seif Aly", "Power of Multi-Talent", "Delve into his life as a passionate filmmaker and watch as he reveals how animated marvels are born from the fusion of media. Anticipate a journey that promises to dazzle your senses!", "Seif Aly, a multi-talented kaleidoscope of creativity, will share his journey as a passionate filmmaker on the TEDx stage. Seif will help you travel into the compelling world of animation; a world where bright landscapes emerge from the blurred boundaries of other media! Await his speech at TEDx 2024!!", "/speakers/seif-aly.png", "seifali.1") }
-                { chainedCard() }
+                { speakerCard("Youssef Sabry", "Behind the scenes of Podcasts", "Tune in to the TEDx stage on February 16th for an auditory adventure with Youssef Sabry, a master podcaster. He'll reveal business secrets and podcasting pearls that promise to turn your daily commute into a riveting masterclass of captivating insights.", "From microphone to mindshare, Youssef Sabry, a podcast owner, will take the TEDx stage on the 16th of February. In a speech centered around business secrets & his podcast, Youssef will definitely transform your commute into a masterclass with his alluring insights! Don't miss his speech!", "/speakers/youssef-sabry.png", "youssefsabry") }
                 { chainedCard() }
                 { chainedCard() }
             </div>
